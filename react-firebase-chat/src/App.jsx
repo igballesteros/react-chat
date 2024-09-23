3b9353062d95bdeb2ce8d7507b0e9a94f6777dda
Ignacio Ballesteros <igbprogramming@gmail.com>
2024-09-23 13:57:57 -0500

    added the sign in and sign up functionality

--- react-firebase-chat/src/App.jsx
+++ react-firebase-chat/src/App.jsx
@@ -3,10 +3,23 @@ import List from "./components/list/List"
 import Detail from "./components/detail/Detail"
 import Login from "./components/login/Login"
 import Notification from "./components/notification/notification"
+import { useEffect } from "react"
+import { onAuthStateChanged } from "firebase/auth"
+import { auth } from "./lib/firebase"
 
 const App = () => {
 
   const user = true
+
+  useEffect(()=> {
+    const unSub = onAuthStateChanged(auth, (user)=>{
+      console.log(user)
+    })
+
+    return () => {
+      unSub();
+    }
+  }, [])
 
   return (
     <div className='container'>
--- react-firebase-chat/src/lib/firebase.js
+++ react-firebase-chat/src/lib/firebase.js
@@ -1,9 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth"
+import { getFirestore } from "firebase/firestore"
+import { getStorage } from "firebase/storage"
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: "reactchat-ec972.firebaseapp.com",
@@ -13,5 +12,8 @@ const firebaseConfig = {
   appId: "1:822784149001:web:c6f1ee392bfe80e1ff1ad8"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth()
+export const db = getFirestore()
+export const storage = getStorage()
